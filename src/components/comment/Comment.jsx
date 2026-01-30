aabb994897b3ab8e843b59167c73eb8e766fe9d5
ebo_o6 <99797833+Ibrahim-Al-Najjar@users.noreply.github.com>
2026-01-30 18:54:11 +0300

    feat(Comment): added voting logic

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,19 +1,66 @@
+import { useState } from "react";
 import replyIcon from "/icon-reply.svg";
+
 function Comment({ score, avatar, username, date, content }) {
+  const [userVote, setUserVote] = useState(0);
+  const [currentScore, setCurrentScore] = useState(score);
+
+  function handleUpVote() {
+    let delta = 0;
+    if (userVote === 1) {
+      delta = -1;
+      setUserVote(0);
+    } else if (userVote === -1) {
+      delta = 2;
+      setUserVote(1);
+    } else {
+      delta = 1;
+      setUserVote(1);
+    }
+    setCurrentScore((s) => s + delta);
+  }
+
+  function handleDownVote() {
+    let delta = 0;
+    if (userVote === -1) {
+      delta = 1;
+      setUserVote(0);
+    } else if (userVote === 1) {
+      delta = -2;
+      setUserVote(-1);
+    } else {
+      delta = -1;
+      setUserVote(-1);
+    }
+    setCurrentScore((s) => s + delta);
+  }
+
   return (
     <div className="w-250 text-[#65696c] p-10 rounded-xl flex gap-10 bg-white">
-      <div className=" font-bold text-[#5152a6] bg-[#f5f6f8] h-fit p-2 rounded-xl">
-        <button className="score-button">+</button>
-        <p>{score}</p>
-        <button className="score-button">-</button>
+      <div className=" font-bold text-[#5152a6] bg-[#f5f6f8] h-fit w-12 p-2 rounded-xl">
+        <button
+          type="button"
+          className={`score-button ${userVote === 1 ? "selected-score-button" : "unselected-score-button"}`}
+          onClick={handleUpVote}
+        >
+          +
+        </button>
+        <p>{currentScore}</p>
+        <button
+          type="button"
+          className={`score-button ${userVote === -1 ? "selected-score-button" : "unselected-score-button"}`}
+          onClick={handleDownVote}
+        >
+          -
+        </button>
       </div>
       <div className="flex flex-col gap-5">
         <div className="flex gap-5 items-center w-full">
-          <img src={avatar} alt="avatar" className="w-10" />
+          <img src={avatar} alt={`${username} avatar`} className="w-10" />
           <p className="text-[#4a5258] font-bold">{username}</p>
           <p>{date}</p>
-          <button className="reply-button">
-            <img src={replyIcon} alt="reply" />
+          <button type="button" className="reply-button">
+            <img src={replyIcon} alt="reply icon" />
             <p className="font-bold">Reply</p>
           </button>
         </div>
