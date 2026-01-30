a7a701d97a6822050e3529ed666ce7b7b17391c5
ebo_o6 <99797833+Ibrahim-Al-Najjar@users.noreply.github.com>
2026-01-30 23:11:53 +0300

    feat(Replies): Showed replies for every comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App() {
   const [comments, setComments] = useState(DATA.comments);
 
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-5 w-250">
       {comments.map((comment) => (
         <Comment
           key={comment.id}
+          id={comment.id}
           score={comment.score}
           username={comment.user.username}
           avatar={comment.user.image.png}
           date={comment.createdAt}
           content={comment.content}
+          replies={comment.replies || []}
         />
       ))}
     </div>
--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import replyIcon from "/icon-reply.svg";
 
-function Comment({ score, avatar, username, date, content }) {
+function Comment({
+  score,
+  avatar,
+  username,
+  date,
+  content,
+  replies = [],
+  classes,
+}) {
   const [userVote, setUserVote] = useState(0);
   const [currentScore, setCurrentScore] = useState(score);
-
+  const [commentReplies, setCommentReplies] = useState(replies);
   function handleUpVote() {
     let delta = 0;
     if (userVote === 1) {
@@ -36,37 +44,54 @@ function Comment({ score, avatar, username, date, content }) {
   }
 
   return (
-    <div className="w-250 text-[#65696c] p-10 rounded-xl flex gap-10 bg-white">
-      <div className=" font-bold text-[#5152a6] bg-[#f5f6f8] h-fit w-12 p-2 rounded-xl">
-        <button
-          type="button"
-          className={`score-button ${userVote === 1 ? "selected-score-button" : "unselected-score-button"}`}
-          onClick={handleUpVote}
-        >
-          +
-        </button>
-        <p>{currentScore}</p>
-        <button
-          type="button"
-          className={`score-button ${userVote === -1 ? "selected-score-button" : "unselected-score-button"}`}
-          onClick={handleDownVote}
-        >
-          -
-        </button>
-      </div>
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-5 items-center w-full">
-          <img src={avatar} alt={`${username} avatar`} className="w-10" />
-          <p className="text-[#4a5258] font-bold">{username}</p>
-          <p>{date}</p>
-          <button type="button" className="reply-button">
-            <img src={replyIcon} alt="reply icon" />
-            <p className="font-bold">Reply</p>
+    <>
+      <div
+        className={` text-[#65696c] p-10 rounded-xl flex gap-10 bg-white ${classes}`}
+      >
+        <div className=" font-bold text-[#5152a6] bg-[#f5f6f8] h-fit w-12 p-2 rounded-xl">
+          <button
+            type="button"
+            className={`score-button ${userVote === 1 ? "selected-score-button" : "unselected-score-button"}`}
+            onClick={handleUpVote}
+          >
+            +
+          </button>
+          <p>{currentScore}</p>
+          <button
+            type="button"
+            className={`score-button ${userVote === -1 ? "selected-score-button" : "unselected-score-button"}`}
+            onClick={handleDownVote}
+          >
+            -
           </button>
         </div>
-        <p className="text-left">{content}</p>
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-5 items-center w-full">
+            <img src={avatar} alt={`${username} avatar`} className="w-10" />
+            <p className="text-[#4a5258] font-bold">{username}</p>
+            <p>{date}</p>
+            <button type="button" className="reply-button">
+              <img src={replyIcon} alt="reply icon" />
+              <p className="font-bold">Reply</p>
+            </button>
+          </div>
+          <p className="text-left">{content}</p>
+        </div>
       </div>
-    </div>
+      <div>
+        {commentReplies.map((reply) => (
+          <Comment
+            classes="w-[80%] ml-auto mb-5"
+            key={reply.id}
+            score={reply.score}
+            username={reply.user.username}
+            avatar={reply.user.image.png}
+            date={reply.createdAt}
+            content={reply.content}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
